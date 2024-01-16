96099b750fbd8ae99c445d4aab5bed48444704af
olawale-o <omoogunolawale@gmail.com>
2024-01-16 07:15:01 +0100

    Add FounderVision component

---
+++ src/app/_components/FounderVision/FounderVision.tsx
@@ -0,0 +1,27 @@
+import style from "./FounderVision.module.css";
+
+const FounderVision = () => {
+  return (
+    <section className={style.vision}>
+      <div className="container">
+        <div className={style.vision_header}>
+          <h2>Chairman{`'`}s Vision</h2>
+        </div>
+        <div className={style.vision_summary}>
+          <div className={style.vision_summary_left}>
+            <p>
+              To build the most innovative and entrepreneurial university in Africa among the top 200 globally, yielding inquiring minds as well as discoveries that are locally and globally impactful.
+            </p>
+            <div className={style.details}>
+              <span className={style.details_name}>Professor Samuel.K Ogidan</span>
+              <span className={style.details_title}>M.Sc(Lancaster) FCCA, FCA, ACTIN</span>
+            </div>
+          </div>
+          <div className={style.img_container}></div>
+        </div>
+      </div>
+    </section>
+  )
+};
+
+export { FounderVision };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
-import { BenefitList } from './_components/Benefits/BenefitList'
+import { BenefitList } from './_components/Benefits/BenefitList';
+import { FounderVision } from './_components/FounderVision/FounderVision';
 import styles from './page.module.css'
 
 export default function Home() {
   return (
     <main className={styles.main}>
+      <FounderVision />
       <BenefitList />
     </main>
   )
