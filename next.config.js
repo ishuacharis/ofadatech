c6f5b7c28bb1aadf2d975ebd74aeb772b195f85b
olawale-o <omoogunolawale@gmail.com>
2024-01-21 06:31:23 +0100

    Fix redirect for basepath

--- next.config.js
+++ next.config.js
@@ -1,4 +1,24 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/about',
+        destination: '/about/leadership',
+        permanent: true,
+      },
+      {
+        source: '/admission',
+        destination: '/admission/requirements',
+        permanent: true,
+      },
+      {
+        source: '/academics',
+        destination: '/academics/engineering',
+        permanent: true,
+      },
+    ]
+  }
+}
 
 module.exports = nextConfig
